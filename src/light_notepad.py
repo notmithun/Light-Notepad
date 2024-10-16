51f48def97cb20b85cded52540fed041e579adef
Mithun <110370009+notmithun@users.noreply.github.com>
2024-10-16 18:59:35 +0530

    Completed the app. Version 1.0 completed

--- src/light_notepad.py
+++ src/light_notepad.py
@@ -13,6 +13,9 @@ from tkinter import messagebox as mbox
 from os import system
 from sys import platform
 import sys
+from tkinter import Menu
+from customtkinter import filedialog as fd
+import webbrowser
 # Imports finish
 
 # Delcaring variables
@@ -38,6 +41,19 @@ Version : {VERSION}
 Python and system version : {sys.version}
 Made by Mithun
 """
+CENTER: str = ctk.CENTER
+
+IS_FILE_OPENED: bool = False
+
+IS_FILE_SAVED: bool = False
+
+global filename
+
+filename: str = ""
+
+txt_file: str = ''
+
+# End of delcaring variables
 
 if platform == "linux" or platform == "linux2" or  platform == "darwin":
     # For Unix-based
@@ -46,6 +62,57 @@ elif platform == "win32":
     # For NT based
     CLS = "cls"
 
+# Functions to be used in buttons or other things
+
+def new():
+    global IS_FILE_OPENED, IS_FILE_SAVED, filename
+    if IS_FILE_OPENED:
+        temp = mbox.askyesnocancel("Save", "Do you want to save the file?")
+        if temp:
+            save(txt_box.get(0.0, "end"), save_file_dialog())
+            txt_box.delete(0.0, "end")
+            filename = ""
+            IS_FILE_SAVED = False
+            IS_FILE_OPENED = False
+        elif temp == None:
+            pass
+        else:
+            txt_box.delete(0.0, "end")
+            IS_FILE_OPENED = False
+            IS_FILE_SAVED = False
+            filename = ""
+    else:
+        txt_box.delete(0.0, "end")
+
+def open_file():
+    global filename
+    global IS_FILE_OPENED
+    IS_FILE_OPENED = True
+    fn = fd.askopenfilename(defaultextension="*.txt, *.log")
+    filename = fn
+    return fn
+
+def save_file_dialog():
+    svd = fd.asksaveasfilename(confirmoverwrite=True, defaultextension="*.txt, *.log")
+    return svd
+
+def set_txt(filename: str="test.txt"):
+    txt_box.delete(0.0, "end")
+    with open (filename, 'r') as read_file:
+        txt_box.insert(index=0.0, text=read_file.read())
+        read_file.close()
+
+def save(information: str="Text....", fn: str="test.txt"):
+    if fn == '':
+        mbox.showerror("Cannot save.", "Error code: 3\n\nFilename is empty. Either use save as or make a new document.")
+        return 1
+    try:
+        txt_saved = open(fn, 'w')
+    except FileNotFoundError as fnfe:
+        mbox.showerror("Error: FileNotFoundError", f"Error message: {fnfe}.\n\n Please recheck that the file is there")
+    txt_saved.writelines(information)
+    txt_saved.close()
+    mbox.showinfo("Successfully saved", "Successfully saved to " + fn)
 
 print(f"Version: {VERSION}")
 
@@ -78,9 +145,44 @@ try:
 # If it fails to find "config.json" then you would have to download from the GitHub page or make your own one.
 except FileNotFoundError as e:
     print(f"Error: {e}")
-    mbox.showerror("Error: FileNotFoundError", f"Error message: {e} \nError Code: 1\n\nHow to fix this error:\n\n1.Make a file named 'config.json' in the directory where the source is located\n\n2.Put this code in there: " + '{"appearance_mode": 1}')
+    mbox.showerror("Error: FileNotFoundError", f"Error message: {e} \nError Code: 1\n\nHow to fix this error:\n\n1.Make a file named 'config.json' in the directory where the source is located\n\n2.Put this code in there: " + '{ "appearance_mode": 1, "start_up_txt": true }')
     exit(1)
+# If it falis to find any of configs.
+except KeyError as ke:
+    print(f"Key Error: {ke}")
+    mbox.showerror("Error: KeyError", f"Error message: {ke} \nError Code: 2\n\nHow to fix this error:\n\n1.In the file 'config.json', erase everything and type this " + '{ "appearance_mode": 1, "start_up_txt": true }')
+    exit(2)
 
 app = ctk.CTk()
+
+app.title(f"Light Notepad - Version {VERSION}")
+app.geometry("400x400")
+
+
+menu = Menu(app)
+app.config(menu=menu)
+
+# File menu
+file_menu = Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+
+
+file_menu.add_cascade(label="New", command=lambda: new())
+file_menu.add_cascade(label="Open", command=lambda: set_txt(open_file()))
+file_menu.add_cascade(label="Save", command=lambda: save(txt_box.get(0.0, "end"), filename))
+file_menu.add_cascade(label="Save as", command=lambda: save(txt_box.get(0.0, "end"), save_file_dialog()))
+
+# Help menu
+help_menu = Menu(menu)
+menu.add_cascade(label="Help", menu=help_menu)
+
+help_menu.add_cascade(label="Open GitHub Wiki", command=lambda: webbrowser.open("https://github.com/notmithun/Light-Notepad/wiki"))
+help_menu.add_cascade(label="Solve errors!", command=lambda: webbrowser.open("https://github.com/notmithun/Light-Notepad/wiki/Error-codes"))
+help_menu.add_separator()
+help_menu.add_cascade(label="About Light Notepad", command=lambda: mbox.showinfo("Light Notepad - Created by Mithun", ABOUT_TXT))
+
+txt_box = ctk.CTkTextbox(app)
+txt_box.pack()
+txt_box.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 app.mainloop()
